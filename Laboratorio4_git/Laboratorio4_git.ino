def7e12c50cc9b5f1d832ba20ad85952f988ebfa
Margareth-Vela <vel19458@uvg.edu.gt>
2023-02-17 15:50:32 -0600

    Cambios en programa de parpaedo de led

--- Laboratorio4_git/Laboratorio4_git.ino
+++ Laboratorio4_git/Laboratorio4_git.ino
@@ -19,7 +19,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   digitalWrite(1, HIGH);
-  delay(1000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW                    // wait for a second
   digitalWrite(1, LOW);
 }
