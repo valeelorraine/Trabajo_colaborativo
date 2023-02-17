c93aeba9da47fd7eab7348305f13b9ca7894fc58
valeelorraine <68074596+valeelorraine@users.noreply.github.com>
2023-02-17 15:30:26 -0600

    Lab4
    Inicio del programa para el laboratorio 4

---
+++ Laboratorio4_git/Laboratorio4_git.ino
@@ -0,0 +1,20 @@
+// Margareth Vela                                  Carné: 19458
+// Valerie Valdez                                  Carné: 19659
+// Universidad del Valle de Guatemala              Sección: 20
+
+//                          Diseño e innovación
+//                          Introducción a GIT
+//                            Laboratorio 4
+
+// Programa para parpadeo de un led y el funcionamiento de un buzzer 
+// de forma intermitente.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
