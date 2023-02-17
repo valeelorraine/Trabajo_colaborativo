5047c0527f4d2b9410bb7a54dd1e73af5ededb74
Margareth-Vela <vel19458@uvg.edu.gt>
2023-02-17 15:47:16 -0600

    Programación para parpadeo de led

--- Laboratorio4_git/Laboratorio4_git.ino
+++ Laboratorio4_git/Laboratorio4_git.ino
@@ -11,10 +11,15 @@
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(1, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(1, HIGH);
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW                    // wait for a second
+  digitalWrite(1, LOW);
 }
